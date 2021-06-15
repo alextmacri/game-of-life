1fe6a798f424e26adaa8c8d2da2aadc24d7407b4
Alex Macri <alexmacri04@gmail.com>
2021-06-15 02:33:12 -0400

    updated temporary logic test file

--- src/game_of_life/logic_test.py
+++ src/game_of_life/logic_test.py
@@ -7,50 +7,79 @@ document where basically the only thing is equivalents to comments and
 docstrings).
 """
 
+DEAD = '.'
+LIVE = '0'
 
-def dead_check(cells, universe):
-    for cell in cells:
-        counter = 0
-
-        for offset in [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]:
-            row, col = cell[1] + offset[1], cell[0] + offset[0]
-            if universe[row][col] == 1:
-                counter += 1
-
-        if not 2 <= counter <= 3:
-            universe[cell[1]][cell[0]] = 0
-
-def live_check(cells, universe):
-    counters = {}
-
-    for cell in cells:
-        for offset in [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]:
-            coords = (cell[0] + offset[0], cell[1] + offset[1])
-
-            if universe[coords[1]][coords[0]] == 0:
-                if coords in counters:
-                    counters[coords] += 1
-                else:
-                    counters[coords] = 1
-
-    for coords, counter in counters.items():
-        if counter == 3:
-            universe[coords[1]][coords[0]] = 1
-
-
-universe = [[0 for _ in range(16)] for _ in range(16)]
+size = 16
+universe = [[DEAD for _ in range(size)] for _ in range(size)]
 cells = []
 
-cells.append((10, 10))
-cells.append((11, 10))
-cells.append((11, 11))
+
+for row in [5, 6]:
+    for col in range(2, 2+9):
+        cells.append((col, row))
+
 
 for cell in cells:
-    universe[cell[1]][cell[0]] = 1
+    universe[cell[1]][cell[0]] = LIVE
 
-dead_check(cells, universe)
-live_check(cells, universe)
+def update():
+    def in_bounds(coordinates: tuple[int]):
+        x = 0 <= coordinates[0] < size
+        y = 0 <= coordinates[1] < size
+        return x and y
+
+    remove_from_cells = []
+    make_dead = []
+    live_surrounding_dead = {}
+
+    offsets = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
+
+    for i, cell in enumerate(cells):
+        dead_surrounding_live = 0
+
+        for offset in offsets:
+            coord = (cell[0] + offset[0], cell[1] + offset[1])
+
+            if in_bounds(coord):
+                if universe[coord[1]][coord[0]] == DEAD:
+                    dead_surrounding_live += 1
+
+                    if coord in live_surrounding_dead:
+                        live_surrounding_dead[coord] += 1
+                    else:
+                        live_surrounding_dead[coord] = 1
+            else:
+                dead_surrounding_live += 1
+
+        if 8 - dead_surrounding_live not in [2, 3]:
+            make_dead.append(cell)
+            # building this list "backwards" so I don't get an IndexError
+            remove_from_cells.insert(0, i)
+
+    for i in remove_from_cells:
+        del cells[i]
+
+    for coord in make_dead:
+        universe[coord[1]][coord[0]] = DEAD
+
+    for coord, counter in live_surrounding_dead.items():
+        if counter == 3 and in_bounds(coord):
+            universe[coord[1]][coord[0]] = LIVE
+            cells.append(coord)
 
 
-for line in universe:
-    print(line)
+def display():
+    for line in universe:
+        for cell in line:
+            print(cell, end=' ')
+        print()
+    print()
+
+
+from time import sleep
+
+while True:
+    display()
+    sleep(0.2)
+    update()

d8182475a552b760840333d418bab16e12a840a4
Alex Macri <alexmacri04@gmail.com>
2021-06-16 16:24:02 -0400

    updated cell images section comment

--- src/game_of_life/assets.py
+++ src/game_of_life/assets.py
@@ -1,6 +1,6 @@
 import pyglet
 
-# Loading the images as variables of the pyglet.image.ImageData type
+# Setting up cell images
 dead_rgba = (30, 30, 30, 255)
 live_rgba = (225, 225, 225, 255)
 cell_dead_image = pyglet.image.SolidColorImagePattern(dead_rgba).create_image(19, 19)

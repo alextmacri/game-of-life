72a8942767ff6e597acbd01cc1b22ae1dbbbf1cc
Alex Macri <alexmacri04@gmail.com>
2021-06-15 20:13:37 -0400

    replaced images with generated solid colors

--- src/game_of_life/assets.py
+++ src/game_of_life/assets.py
@@ -1,5 +1,7 @@
 import pyglet
 
 # Loading the images as variables of the pyglet.image.ImageData type
-cell_dead_image = pyglet.image.load('src/assets/black_square.png')
-cell_live_image = pyglet.image.load('src/assets/white_square.png')
+dead_rgba = (30, 30, 30, 255)
+live_rgba = (225, 225, 225, 255)
+cell_dead_image = pyglet.image.SolidColorImagePattern(dead_rgba).create_image(19, 19)
+cell_live_image = pyglet.image.SolidColorImagePattern(live_rgba).create_image(19, 19)

538e270c0914d4d1a227579c9853075d58c17d71
Alex Macri <alexmacri04@gmail.com>
2021-06-20 03:52:12 -0400

    added documentation to Target.py

--- src/game_of_life/scenes/Target.py
+++ src/game_of_life/scenes/Target.py
@@ -4,15 +4,17 @@ from typing import Callable
 from game_of_life.ModeState import ModeState
 
 class Target(SceneInterface):
+    """
+    (this hasn't been started yet)
+    """
+
     def __init__(self, switch_scene_cb: Callable[[str], None], switch_mode_cb: Callable[[ModeState], None]) -> None:
+        """"""
         self.__switch_scene_cb = switch_scene_cb
         self.__switch_mode_cb = switch_mode_cb
         self.__is_active = False
 
         self.__main_batch = pyglet.graphics.Batch()
-        self.__cell_group = pyglet.graphics.OrderedGroup(1)
-        self.__backdrop_group = pyglet.graphics.OrderedGroup(2)
-        self.__ui_group = pyglet.graphics.OrderedGroup(3)
 
     def set_is_active(self, is_active: bool) -> None:
         """"""

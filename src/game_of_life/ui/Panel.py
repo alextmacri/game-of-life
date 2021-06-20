ce663055438be5a3b9b23288d0bd84d8daf65395
Alex Macri <alexmacri04@gmail.com>
2021-06-20 03:52:24 -0400

    added documentation to Panel.py

--- src/game_of_life/ui/Panel.py
+++ src/game_of_life/ui/Panel.py
@@ -3,9 +3,29 @@ from game_of_life.assets import backdrop_image_generator
 from game_of_life.ui.Button import Button
 
 class Panel:
-    """"""
+    """
+    Represents a button in the pyglet window of the game.
+
+    is_showing: bool, the state of the panel being updated and rendered or not
+    x_cor: int, the x coordinates of the left of the button when drawn in the
+    game window
+    y_cor: int, the y coordinates of the bottom of the button when drawn in the
+    game window
+    batch: pyglet.graphics.Batch, the pyglet rendering batch that the panel's
+    ui element belongs to
+    backdrop_group: pyglet.graphics.OrderedGroup, the pyglet rendering group that the
+    panel's backdrop image sprite belongs to
+    button_group: pyglet.graphics.OrderedGroup, the pyglet rendering group that the
+    panel's button image sprites belong to
+    text_group: pyglet.graphics.OrderedGroup, the pyglet rendering group that the
+    panel's text belongs to
+    label: pyglet.text.Label, the title text of the panel
+    continue_button: Button, the button to close the pause panel and unpause
+    exit_button: Button, the button to return you to the main menu
+    """
 
     def __init__(self, x_cor: int, y_cor: int, text: str, panel_batch: pyglet.graphics.Batch, backdrop_group: pyglet.graphics.OrderedGroup, button_group: pyglet.graphics.OrderedGroup, text_group: pyglet.graphics.OrderedGroup) -> None:
+        """Initialize this Panel."""
         self.__is_showing = False
 
         width = 220
@@ -59,7 +79,9 @@ class Panel:
         )
 
     def get_is_showing(self) -> bool:
+        """Return the value of is_pressed."""
         return self.__is_showing
 
     def set_is_showing(self, is_showing: bool) -> None:
+        """Set the value of is_showing (attribute) to is_showing (argument)."""
         self.__is_showing = is_showing

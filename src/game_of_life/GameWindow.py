70b88b6135e7f4bd20af085411aff9cf7c68ec56
Alex Macri <alexmacri04@gmail.com>
2021-06-20 03:51:20 -0400

    added documentation to GameWindow.py

--- src/game_of_life/GameWindow.py
+++ src/game_of_life/GameWindow.py
@@ -6,7 +6,16 @@ from game_of_life.scenes.Target import Target
 from game_of_life.scenes.Free import Free
 
 class GameWindow(pyglet.window.Window):
+    """
+    Represents a pyglet window of the game.
+
+    modes: dict[ModeState, SceneController], the dictionary of game modes
+    active_mode: SceneController, the mode currently being displayed and updated
+    background: the background sprite of the entire pyglet window
+    """
+
     def __init__(self, *args, **kwargs):
+        """Initialize this GameWindow."""
         win_width, win_height = 860, 640
         super(GameWindow, self).__init__(win_width, win_height, *args, **kwargs)
 
@@ -29,22 +38,52 @@ class GameWindow(pyglet.window.Window):
         }
         self.__active_mode = self.modes[ModeState.MENU]
 
-        __background_color = (80, 80, 80, 255)
-        __background_image_data = pyglet.image.SolidColorImagePattern(__background_color).create_image(win_width, win_height)
-        self.__background = pyglet.sprite.Sprite(__background_image_data, x=0, y=0)
+        # Setting up and creating the background sprite
+        background_color = pyglet.image.SolidColorImagePattern(
+            (80, 80, 80, 255)
+        )
+        background_image_data = background_color.create_image(
+            win_width,
+            win_height
+        )
+        self.__background = pyglet.sprite.Sprite(
+            background_image_data,
+            x=0,
+            y=0
+        )
 
-    def __switch_mode(self, mode_state: ModeState):
+    def __switch_mode(self, mode_state: ModeState) -> None:
+        """
+        Switch the current mode to the mode of mode_state by updating the active
+        mode.
+        """
         self.__active_mode = self.modes[mode_state]
 
-    def on_mouse_press(self, x: int, y: int, *_):
+    def on_mouse_press(self, x: int, y: int, *_) -> None:
+        """
+        Catch the mouse press event with the active mode's active scene's event
+        handler for a mouse press.
+        """
         self.__active_mode.active_scene.mouse_press(x, y)
 
-    def on_mouse_release(self, x: int, y: int, *_):
+    def on_mouse_release(self, x: int, y: int, *_) -> None:
+        """
+        Catch the mouse release event with the active mode's active scene's
+        event handler for a mouse release.
+        """
         self.__active_mode.active_scene.mouse_release(x, y)
 
-    def on_mouse_drag(self, x: int, y: int, *_):
+    def on_mouse_drag(self, x: int, y: int, *_) -> None:
+        """
+        Catch the mouse drag event with the active mode's active scene's event
+        handler for a mouse drag.
+        """
         self.__active_mode.active_scene.mouse_drag(x, y)
 
-    def on_draw(self):
+    def on_draw(self) -> None:
+        """
+        Draw the background sprite and call the active mode's active scene's
+        draw method.
+        """
         self.__background.draw()
         self.__active_mode.active_scene.draw()

28f5c13a647abc91f72d6cc9f39c025c2e36d2ef
Alex Macri <alexmacri04@gmail.com>
2021-06-20 00:19:01 -0400

    added backdrop image generator to assets.py

--- src/game_of_life/assets.py
+++ src/game_of_life/assets.py
@@ -6,6 +6,11 @@ live_rgba = (225, 225, 225, 255)
 cell_dead_image = pyglet.image.SolidColorImagePattern(dead_rgba).create_image(19, 19)
 cell_live_image = pyglet.image.SolidColorImagePattern(live_rgba).create_image(19, 19)
 
+# Setting up backdrop image generator
+backdrop_rgba = (180, 100, 100, 180)
+def backdrop_image_generator(width: int, height: int) -> pyglet.image.ImageData:
+    return pyglet.image.SolidColorImagePattern(backdrop_rgba).create_image(width, height)
+
 # Loading button images
 button_pressed_image = pyglet.image.load('src/assets/button_pressed.png')
 button_unpressed_image = pyglet.image.load('src/assets/button_unpressed.png')

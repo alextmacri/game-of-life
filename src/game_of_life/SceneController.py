9f210570f7686df2e3b8706913e68778462281e4
Alex Macri <alexmacri04@gmail.com>
2021-06-20 03:51:35 -0400

    added documentation to SceneController.py

--- src/game_of_life/SceneController.py
+++ src/game_of_life/SceneController.py
@@ -3,14 +3,29 @@ from game_of_life.ModeState import ModeState
 from game_of_life.SceneInterface import SceneInterface
 
 class SceneController:
-    def __init__(self, switch_mode_cb: Callable[[ModeState], None], scenes: dict[str, SceneInterface], active_scene: str):
-        self.__switch_mode_cb = switch_mode_cb
+    """
+    Represents a mode, or holder and controller of scenes in the game.
 
+    switch_mode_cb: Callable[[ModeState], None], the callable function from
+    GameWindow to switch modes
+    """
+
+    def __init__(self, switch_mode_cb: Callable[[ModeState], None],
+                 scenes: dict[str, SceneInterface], active_scene: str):
+        """Initialize this Button."""
+        # Initializing each SceneInterface object that was passed in, so I can
+        # pass in the callbacks
         for scene_key in scenes:
-            scenes[scene_key] = scenes[scene_key](self.__switch_scene, self.__switch_mode_cb)
+            scenes[scene_key] = scenes[scene_key](
+                self.__switch_scene,
+                switch_mode_cb
+            )
         self.scenes = scenes
         self.__switch_scene(active_scene)
     
     def __switch_scene(self, scene_name: str):
+        """
+        Switch the current scene to the scene of scene_state by updating the
+        active scene.
+        """
         self.active_scene = self.scenes[scene_name]
-        self.active_scene.set_is_active(True)

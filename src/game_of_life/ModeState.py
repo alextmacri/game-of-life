ba0e65b04228f0ec747f2c998decc0807079e1a6
Alex Macri <alexmacri04@gmail.com>
2021-06-20 03:51:27 -0400

    added documentation to ModeState.py

--- src/game_of_life/ModeState.py
+++ src/game_of_life/ModeState.py
@@ -1,4 +1,13 @@
 class ModeState:
+    """
+    Enum that represents the modes of the pyglet app.
+
+    MENU: represents the menu mode
+    FREE: represents the free play mode
+    TARGET: represents the target mode
+    EDIT: represents the edit mode
+    """
+
     MENU = 0
     FREE = 1
     TARGET = 2

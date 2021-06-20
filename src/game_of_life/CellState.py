70e1b6367b21326e50b194ceda27ecc242e68739
Alex Macri <alexmacri04@gmail.com>
2021-06-20 03:51:13 -0400

    added documentation to CellState.py

--- src/game_of_life/CellState.py
+++ src/game_of_life/CellState.py
@@ -1,3 +1,10 @@
 class CellState:
+    """
+    Enum that represents the states of a cell in a universe.
+
+    DEAD: the state of a dead cell
+    LIVE: the state of a live cell
+    """
+
     DEAD = 0
     LIVE = 1
